d7fa422009eade9c1634ddb647584e8b84af2c82
ayanh <ayan.howc@gmail.com>
2023-04-04 22:07:28 +0200

    update api

--- app/src/main/java/com/example/projet_e5_version_final/InscrptionActivity.java
+++ app/src/main/java/com/example/projet_e5_version_final/InscrptionActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Inscrption extends AppCompatActivity {
+public class InscrptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
