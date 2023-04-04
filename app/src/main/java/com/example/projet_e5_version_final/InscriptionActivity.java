fc4c095e1f630b89d0a8edb5189d0ba2b7865c3e
ayanh <ayan.howc@gmail.com>
2023-04-04 23:40:08 +0200

    update inscription

--- app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
+++ app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class InscrptionActivity extends AppCompatActivity {
+public class InscriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
