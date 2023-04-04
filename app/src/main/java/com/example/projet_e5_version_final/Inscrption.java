d9f21bf8c7c4da1c7f5007928252df98980a816a
ayanh <ayan.howc@gmail.com>
2023-04-04 20:57:48 +0200

    commit ayan

---
+++ app/src/main/java/com/example/projet_e5_version_final/Inscrption.java
@@ -0,0 +1,14 @@
+package com.example.projet_e5_version_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inscrption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscrption);
+    }
+}
