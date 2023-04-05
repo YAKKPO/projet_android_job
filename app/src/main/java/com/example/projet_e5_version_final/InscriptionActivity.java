daba2d6b8d2f3eb3da07bd1e1060df76d109dd3a
HZY552 <houzeyu7@gmail.com>
2023-04-05 22:09:29 +0200

    add spinner dialog

--- app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
+++ app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
@@ -1,8 +1,19 @@
 package com.example.projet_e5_version_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InscriptionActivity extends AppCompatActivity {
 
@@ -10,5 +21,54 @@ public class InscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        TextView tv_nom = findViewById(R.id.nom);
+        set_Spinner();
+
+        tv_nom.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                int width = tv_nom.getWidth();
+                int height = tv_nom.getHeight();
+
+
+                Spinner spinner = findViewById(R.id.genre);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+                layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+                layoutParams.bottomMargin = dpToPx(20);
+                spinner.setLayoutParams(layoutParams);
+                spinner.setDropDownWidth(-1);
+                tv_nom.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
     }
+
+    protected void set_Spinner(){
+        Spinner spinner = findViewById(R.id.genre);
+
+        String[] items = new String[]{"M. ", "F. "};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private int dpToPx(int dp) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
 }
