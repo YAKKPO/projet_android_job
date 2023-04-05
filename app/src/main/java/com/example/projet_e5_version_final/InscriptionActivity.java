79ac48557b7b7cd5c55e4d2f33dbcc5a1d31703c
ayanh <ayan.howc@gmail.com>
2023-04-05 19:12:10 +0200

    update mon compte

--- app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
+++ app/src/main/java/com/example/projet_e5_version_final/InscriptionActivity.java
@@ -9,6 +9,6 @@ public class InscriptionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inscrption);
+        setContentView(R.layout.activity_inscription);
     }
 }
