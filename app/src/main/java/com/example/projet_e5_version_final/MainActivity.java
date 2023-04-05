cd62fb0cd81d85a4408f988d5f504d57478bf794
HZY552 <houzeyu7@gmail.com>
2023-04-05 20:44:26 +0200

    update api

--- app/src/main/java/com/example/projet_e5_version_final/MainActivity.java
+++ app/src/main/java/com/example/projet_e5_version_final/MainActivity.java
@@ -12,16 +12,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.util.DisplayMetrics;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.projet_e5_version_final.adapters.AdapterHitorique;
+import com.example.projet_e5_version_final.adapters.AdapterShowLogin;
+import com.example.projet_e5_version_final.adapters.AdapterShowRDV;
 import com.example.projet_e5_version_final.services.Api;
 import com.google.android.material.internal.NavigationMenu;
 import com.google.android.material.navigation.NavigationView;
 import androidx.appcompat.widget.Toolbar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -98,27 +109,83 @@ public class MainActivity extends AppCompatActivity {
         Intent info_login = getIntent();
         id = info_login.getStringExtra("id");
         type = info_login.getStringExtra("type");
-        System.out.println(id + type);
+
         if (id != null && type != null){
-            String values = "{id:" + id + "}";
-            ArrayList<String> listValues = new ArrayList<>(Arrays.asList("find_user_By_id", "None", "Jiojio000608.", values));
-            Api api = new Api(listValues);
-            api.start();
-            change_menu_user(navigationView);
             try {
-                api.join();
-                JSONObject api_json = new JSONObject(api.get_Values());
-
-                TextView tv_titie1 = findViewById(R.id.main_title1);
-                TextView tv_title2 = findViewById(R.id.main_title2);
-
-                tv_titie1.setText("Bonjour !");
-                tv_title2.setText(api_json.getString("first_name") + " " + api_json.getString("last_name"));
-
-            } catch (InterruptedException | JSONException e) {
+                change_title(navigationView);
+                show_historique();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
+        }else{
+            ArrayList list = new ArrayList<>();
+            list.add(0,"");
+            ListView list_View_historique_non = findViewById(R.id.list_historique);
+
+            BaseAdapter adapterShowLogin = new AdapterShowLogin(this,list,this);
+            list_View_historique_non.setAdapter(adapterShowLogin);
+
         }
+
+
+    }
+
+    protected void show_historique() throws InterruptedException, JSONException {
+        String values = "{id_patient:" + id + "}";
+        ArrayList<String> listValues = new ArrayList<>(Arrays.asList("find_historique_By_patientId", "None", "Jiojio000608.", values));
+        Api api = new Api(listValues);
+        api.start();
+
+        api.join();
+
+        JSONArray jsonArray = new JSONArray(api.get_Values());
+        if (jsonArray.length() > 0){
+            ListView list_View_historique = findViewById(R.id.list_historique);
+
+            BaseAdapter adapter_historique = new AdapterHitorique(this,jsonArray);
+            list_View_historique.setAdapter(adapter_historique);
+
+            list_View_historique.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    JSONObject obj = null;
+                    try {
+                        obj = new JSONObject(adapterView.getItemAtPosition(i).toString());
+                        Toast.makeText(MainActivity.this, obj.getString("email"), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+            });
+        }else{
+            ArrayList list = new ArrayList<>();
+            list.add(0,"");
+            ListView list_View_historique_non = findViewById(R.id.list_historique);
+
+            BaseAdapter adapterShowRDV = new AdapterShowRDV(this,list,this);
+            list_View_historique_non.setAdapter(adapterShowRDV);
+        }
+
+    }
+
+    protected void change_title(NavigationView navigationView) throws InterruptedException, JSONException {
+        String values = "{id:" + id + "}";
+        ArrayList<String> listValues = new ArrayList<>(Arrays.asList("find_user_By_id", "None", "Jiojio000608.", values));
+        Api api = new Api(listValues);
+        api.start();
+        change_menu_user(navigationView);
+
+        api.join();
+        JSONObject api_json = new JSONObject(api.get_Values());
+
+        TextView tv_titie1 = findViewById(R.id.main_title1);
+        TextView tv_title2 = findViewById(R.id.main_title2);
+
+        tv_titie1.setText("Bonjour !");
+        tv_title2.setText(api_json.getString("first_name") + " " + api_json.getString("last_name"));
 
 
     }
